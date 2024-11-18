a7d4f00fe9ebd4bc475f7736327dfb693da41045
namanjain0007 <namanjainayushjain7@gmail.com>
2024-11-18 11:54:43 +0530

    Update auth-controllers.js

--- server/controllers/auth-controllers.js
+++ server/controllers/auth-controllers.js
@@ -86,6 +86,7 @@ const login = async (req, res) => {
   httpOnly: true,
   secure: true, // HTTPS ke liye zaroori
   sameSite: "None", // Cross-origin ke liye
+  path: "/",
 });
 
       // console.log("ayush", res.getHeaders());
